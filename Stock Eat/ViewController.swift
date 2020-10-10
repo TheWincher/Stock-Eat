6e456983521f0b919aa27124e89d092e5a1baeb3
Louchez Emmanuel <louchez26@hotmail.fr>
2020-10-10 16:53:14 +0200

    Initial Commit

---
+++ Stock Eat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stock Eat
+//
+//  Created by Emmanuel LOUCHEZ on 10/10/2020.
+//  Copyright © 2020 Emmanuel LOUCHEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Stock EatTests/Stock_EatTests.swift
@@ -0,0 +1,34 @@
+//
+//  Stock_EatTests.swift
+//  Stock EatTests
+//
+//  Created by Emmanuel LOUCHEZ on 10/10/2020.
+//  Copyright © 2020 Emmanuel LOUCHEZ. All rights reserved.
+//
+
+import XCTest
+@testable import Stock_Eat
+
+class Stock_EatTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
